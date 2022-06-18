55c9b9af1a40ba1502060b99bfe48f48fef32078
shui-clouds <57289318+shui-clouds@users.noreply.github.com>
2022-06-18 23:28:15 +0100

    enh: updated types for agent actions

--- lib/types.ts
+++ lib/types.ts
@@ -25,13 +25,20 @@ type VMIdentification = {
 }
 
 interface ActionsMap {
+	wakeHost: Record<string, unknown>,
+	update: Record<string, unknown>,
 	shutDownHost: Record<string, unknown>,
+	suspendHost: Record<string, unknown>,
+	restartHost: Record<string, unknown>,
+	restartHostParsec: Record<string, unknown>,
+	start: VMIdentification,
+	stop: VMIdentification,
 	setCPU: VMIdentification & {
 		value: number
 	},
-	other: {
-		a: number[]
-	}
+	setRAM: VMIdentification & {
+		value: number
+	},
 }
 
 export type ActionName = keyof ActionsMap
@@ -39,11 +46,3 @@ export type ActionName = keyof ActionsMap
 export type ActionBody<A extends ActionName> = {
 	action: A
 } & ActionsMap[A]
-
-const test = <A extends ActionName>(o: ActionBody<A>) => {}
-
-test({
-	action: 'shutDownHost',
-})
-
-type AgentCommand = 'shutdownHost' | 'suspendHost' | 'restartHost' | 'restartHostParsec' | 'setCPU' | 'setRAM' | 'update' | 'start' | 'stop'
