7572e428528a31e97df40add3b346284e834c759
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-06-22 16:27:18 +0100

    refactor: move socketio client to its own hook

---
+++ apps/web/src/client/lib/useStatus.ts
@@ -0,0 +1,35 @@
+import type {Status} from 'types'
+import {useEffect, useState} from 'react'
+
+import {io} from 'socket.io-client'
+
+export function useStatus(): [boolean, Status] {
+	const [connected, setConnected] = useState(false)
+	const [status, setStatus] = useState<Status>()
+
+	useEffect(() => {
+		const socket = io()
+
+		socket.on('connect', () => {
+			setConnected(true)
+		})
+
+		socket.on('status', (updatedStatus) => {
+			setStatus(updatedStatus)
+		})
+
+		socket.on('disconnect', () => {
+			setConnected(false)
+			setStatus(undefined)
+		})
+
+		socket.on('agentOffline', () => {
+			setStatus(undefined)
+		})
+
+		return () => { socket.close() }
+	}, [])
+
+	// loading, latestStatus
+	return [!connected, status as Status]
+}
