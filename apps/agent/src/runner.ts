4b6ba5b4e0d83d46f177b56ca815a23302b91f41
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-06-23 13:05:41 +0100

    fix(agent): prod tsconfig

--- apps/agent/src/runner.ts
+++ apps/agent/src/runner.ts
@@ -1,6 +1,6 @@
 import {ChildProcess, spawn, SpawnOptions} from 'child_process'
 
-const cmd = 'ts-node -T src/'
+const cmd = 'ts-node -P tsconfig.prod.json -T src/'
 
 const options: SpawnOptions = {
 	shell: true,
