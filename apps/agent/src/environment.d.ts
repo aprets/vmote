66dabead3fc1c3aa846ef7abb58d18703769335d
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-06-23 11:04:01 +0100

    feat(agent): remove old env, add DEV mode env

--- apps/agent/src/environment.d.ts
+++ apps/agent/src/environment.d.ts
@@ -1,11 +1,10 @@
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
-			GIT_REPO_URL: string
-			GIT_BRANCH: string
-			CHECKIN_POST_URL: string
-			NORMAL_POWER_PLAN_GUID: string
-			SLEEPY_POWER_PLAN_GUID: string
+			DEV: string | undefined,
+			CHECKIN_POST_URL: string,
+			NORMAL_POWER_PLAN_GUID: string,
+			SLEEPY_POWER_PLAN_GUID: string,
 		}
 	}
 }
